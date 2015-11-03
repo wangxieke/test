c68d371d940716323dd15a52a6c449e50e9d50c0
chris <wangxieke99@163.com>
2015-11-03 16:22:42 +0800

    test

---
+++ test01/src/com/test/TestGit.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class TestGit {
+
+	private String Id;
+
+	private String value;
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
